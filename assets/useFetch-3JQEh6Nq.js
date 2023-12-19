127698928080210e4e1486756a2694fbfcb33098
Michael Brown <varstalshitsya@gmail.com>
2023-12-19 17:49:39 +0300

    2-nd build try

--- assets/useFetch-3JQEh6Nq.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./index-M6l4noiw.js";function c(r,n){const t=u(n);function e(){r().then(o=>{t.value=o})}return e(),{data:t,refetch:e}}export{c as u};
---
+++ assets/useFetch-hGF0LQEk.js
@@ -0,0 +1,1 @@
+import{r as u}from"./index-T_nD_F5y.js";function c(r,n){const t=u(n);function e(){r().then(o=>{t.value=o})}return e(),{data:t,refetch:e}}export{c as u};
